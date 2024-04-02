fadf14e5f97cee42555a9b1b3b08938377b6deb0
nttminh <ng.minh@hotmail.com>
2024-04-02 15:50:36 -0400

    reconfigure Plasmo Config on URL

--- contents/content.ts
+++ contents/content.ts
@@ -1,9 +1,9 @@
-import type { PlasmoCSConfig } from "plasmo";
+import type { PlasmoCSConfig } from "plasmo"
 
-import { sendToBackground } from "@plasmohq/messaging";
+import { sendToBackground } from "@plasmohq/messaging"
 
 export const config: PlasmoCSConfig = {
-  // matches: ["https://campus.sa.umasscs.net/psp/csm/EMPLOYEE/*"],
+  matches: ["https://campus.sa.umasscs.net/psp/csm/EMPLOYEE/*"],
   all_frames: true
 }
 
@@ -13,13 +13,15 @@ function updateAndAddRatings(span, metaData) {
 
   metaData.forEach((data, index) => {
     // Create a text node for the instructor's name and append it
-    const name = document.createTextNode(data.firstName + (data.lastName ? " " + data.lastName : ""));
-    span.appendChild(name);
+    const name = document.createTextNode(
+      data.firstName + (data.lastName ? " " + data.lastName : "")
+    )
+    span.appendChild(name)
 
     // Create a link (anchor) element
-    const anchor = document.createElement("a");
-    anchor.className = "rate-my-professor-link";
-    anchor.target = "_blank";
+    const anchor = document.createElement("a")
+    anchor.className = "rate-my-professor-link"
+    anchor.target = "_blank"
 
     // Set text content and href based on whether legacyId is available
     if (data.legacyId === null || data.avgRating === 0) {
@@ -51,71 +53,75 @@ function modernViewDOMHandler() {
     const iframeDocument =
       iframe.contentDocument || iframe.contentWindow.document
     // fix the above line
-    iframeDocument.querySelectorAll('span[id^="MTG_INSTR$"]').forEach(async function (span: HTMLSpanElement) {
-      // Skip iteration if the instructor name is "To Be Announced"
-      if (span.innerText === "To be Announced") {
-        return true // true to bypass the warning
-      }
-
-      // Create an array to store the results for instructors
-      const multiRes = []
-
-      // If the name contains a comma, it's a list of instructors
-      if (span.innerText.includes(",")) {
-        const instructorNames = span.innerText.split(",\n")
-        for (const instructorName of instructorNames) {
-          let res = await sendToBackground({
-            name: "getTeacher",
-            body: {
-              teacherName: instructorName,
-              schoolId: "U2Nob29sLTM5ODA="
-            },
-            extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
-          })
-
-          // if null is returned, manually create a instructor object
-          if (res === null) {
-            res = {
-              avgRating: null,
-              legacyId: null,
-              firstName: instructorName.split(" ")[0],
-              lastName: instructorName.split(" ")[1]
-            }
-          }
-          // save the results for each instructor
-          multiRes.push(res)
+    iframeDocument
+      .querySelectorAll('span[id^="MTG_INSTR$"]')
+      .forEach(async function (span: HTMLSpanElement) {
+        // Skip iteration if the instructor name is "To Be Announced"
+        if (span.innerText === "To be Announced") {
+          return true // true to bypass the warning
         }
-        // console.log(multiRes);
+
+        // Create an array to store the results for instructors
+        const multiRes = []
+
+        // If the name contains a comma, it's a list of instructors
+        if (span.innerText.includes(",")) {
+          const instructorNames = span.innerText.split(",\n")
+          for (const instructorName of instructorNames) {
+            let res = await sendToBackground({
+              name: "getTeacher",
+              body: {
+                teacherName: instructorName,
+                schoolId: "U2Nob29sLTM5ODA="
+              },
+              extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
+            })
+
+            // if null is returned, manually create a instructor object
+            if (res === null) {
+              res = {
+                avgRating: null,
+                legacyId: null,
+                firstName: instructorName.split(" ")[0],
+                lastName: instructorName.split(" ")[1]
+              }
+            }
+            // save the results for each instructor
+            multiRes.push(res)
+          }
+          // console.log(multiRes);
+          updateAndAddRatings(span, multiRes)
+          return multiRes
+        }
+
+        // For a single professor
+        let res = await sendToBackground({
+          name: "getTeacher",
+          body: {
+            teacherName: span.innerText,
+            schoolId: "U2Nob29sLTM5ODA="
+          },
+          extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
+        })
+        if (res === null) {
+          res = {
+            avgRating: null,
+            legacyId: null,
+            firstName: span.innerText,
+            lastName: null
+          }
+        }
+        multiRes.push(res)
         updateAndAddRatings(span, multiRes)
         return multiRes
-      }
-
-      // For a single professor
-      let res = await sendToBackground({
-        name: "getTeacher",
-        body: {
-          teacherName: span.innerText,
-          schoolId: "U2Nob29sLTM5ODA="
-        },
-        extensionId: process.env.PLASMO_PUBLIC_EXTENSION_ID
       })
-      if (res === null) {
-        res = {
-          avgRating: null,
-          legacyId: null,
-          firstName: span.innerText,
-          lastName: null
-        }
-      }
-      multiRes.push(res)
-      updateAndAddRatings(span, multiRes)
-      return multiRes
-    })
   }
 }
 
 function classicViewDOMHandler() {
-  document.querySelectorAll('span[id^="MTG_INSTR$"]').forEach(async function (span: HTMLSpanElement) {
+  document.querySelectorAll('span[id^="MTG_INSTR$"]').forEach(async function (
+    span: HTMLSpanElement
+  ) {
     if (span.getAttribute("data-processed")) {
       // Skip this span as it has already been processed
       return
@@ -177,7 +183,7 @@ function classicViewDOMHandler() {
         lastName: null
       }
     }
-    
+
     multiRes.push(res)
     updateAndAddRatings(span, multiRes)
     return multiRes
